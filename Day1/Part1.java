6c6bd22f174614e15f16b9e73635e617261d447b
vaishakkmenon <vmenon19@uab.edu>
2022-12-01 09:38:49 -0600

    Day 1 Part 1 done

--- Day1/Part1.java
+++ Day1/Part1.java
@@ -1,9 +1,56 @@
 package Day1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Part1
 {
     public static void main(String[] args)
     {
+        File f = new File("Part1Data.txt");
+        int total = 0;
+        ArrayList<Integer> elves = new ArrayList<Integer>();
+        try 
+        {
+            Scanner sc = new Scanner(f);
+            while(sc.hasNextLine())
+            {
+                String line = sc.nextLine();
+                if(line != "")
+                {
+                    int num = Integer.parseInt(line);
+                    total += num;
+                }
+                else
+                {
+                    elves.add(total);
+                    total = 0;
+                }
+            }
 
+            sc.close();
+        } 
+        catch (FileNotFoundException e) 
+        {
+            e.printStackTrace();
+        }
+
+        int max = -1;
+        int index = -1;
+        int count = 0;
+        for(int i : elves)
+        {
+            if(i > max)
+            {
+                max = i;
+                index = count;
+            }
+            count++;
+        }
+        
+        System.out.println("Elf with the most amount of calories: " + index);
+        System.out.println("Number of calories elf " + index + " carries:" + max);
     }
 }
